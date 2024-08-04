c25deb972cfc09973ab94589784dac075b271eb2
library <2804045156@qq.com>
2024-08-04 16:36:05 +0800

    逻辑过期解决商户缓存击穿

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,35 @@
 package com.hmdp;
 
+import com.hmdp.service.impl.ShopServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
-class HmDianPingApplicationTests {
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static cn.hutool.core.lang.Console.log;
+
+@SpringBootTest()
+@RunWith(SpringJUnit4ClassRunner.class)
+public class HmDianPingApplicationTests {
 
 
+    @Resource
+    private ShopServiceImpl shopService;
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+    @Test
+    public void testSave() throws InterruptedException {
+        shopService.saveShop2Redis(1L,10L);
+        Map<Object, Object> shopRedisMap = stringRedisTemplate.opsForHash().entries("cache:shop:1");
+
+        // 存在 判断是否过期
+
+        System.out.println(LocalDateTime.now());
+        System.out.println(shopRedisMap.get("expireTime"));
+    }
 }
